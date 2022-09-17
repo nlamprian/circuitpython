0853048534ab493a3a51c555bf4f12671204cbf2
Nick Lamprianidis <info@nlamprian.me>
2022-09-17 21:41:55 +0200

    Include upip with W5100S EVB Pico firmware

---
+++ ports/rp2/boards/W5100S_EVB_PICO/manifest.py
@@ -0,0 +1,4 @@
+include("../manifest.py")
+
+module("upip.py", base_path="$(MPY_DIR)/tools", opt=3)
+module("upip_utarfile.py", base_path="$(MPY_DIR)/tools", opt=3)
--- ports/rp2/boards/W5100S_EVB_PICO/mpconfigboard.cmake
+++ ports/rp2/boards/W5100S_EVB_PICO/mpconfigboard.cmake
@@ -2,3 +2,6 @@
 set(PICO_BOARD wiznet_w5100s_evb_pico)
 set(MICROPY_PY_NETWORK_WIZNET5K W5100S)
 set(MICROPY_PY_LWIP 1)
+
+# Board specific version of the frozen manifest
+set(MICROPY_FROZEN_MANIFEST ${CMAKE_SOURCE_DIR}/boards/W5100S_EVB_PICO/manifest.py)
